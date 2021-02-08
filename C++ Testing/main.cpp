d54da5bbb5bb42b2458c48629165871ec1ab22b3
CCheukKa <ccheukka@gmail.com>
2021-02-08 14:28:32 +0800

    deleted trash

--- C++ Testing/main.cpp
+++ C++ Testing/main.cpp
@@ -13,30 +13,6 @@ using namespace std;
 
 int main(){
 
-    int number, goal;
-    int sum;
-    cin >> number >> goal;
-
-    if( goal >= 0 ){
-        for (int i = 0; i < number - 1; i++)
-        {
-            int n = floor(goal / (number - 1)) + i;
-            sum += n;
-            cout << n;
-            cout << " ";
-        }
-        cout << goal - sum;
-    } else {
-        for (int i = 0; i < number - 1; i++)
-        {
-            int n = floor(goal * -1 / (number - 1)) + i;
-            sum += n;
-            cout << n * -1;
-            cout << " ";
-        }
-        cout << goal + sum;
-    }
-
     return 0;
 }
 
