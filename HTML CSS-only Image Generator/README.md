f1c0a57f2d45eb33c0d8c6428baf919140195e28
CCheukKa <ccheukka@gmail.com>
2021-07-15 21:36:45 +0800

    Added CSS-only image generator

---
+++ HTML CSS-only Image Generator/README.md
@@ -0,0 +1,11 @@
+This project generates a CSS-only image renderer.
+
+# Instructions
+1. Make sure you have node.js installed.
+2. Navigate to `./config.json` and tinker with the stuff inside to your heart's content. `filePath` could be a URL, or a relative path to a local file.
+3. Run `node .` in this root directory.
+4. Install the missing dependencies if there is any, go back to step 3 after installing them.
+5. Launch `./index.html`, it should be an example project of the CSS-only image.
+6. Copy `./integrated.html` only, or `./index.html` and `./stylesheet.css` into your project. Use them however you wish.
+
+Note that all the other files are only necessary in the process of `stylesheet.css` creation and can be discarded once you no longer need to generate more CSS-only images.
---
+++ HTML CSS-only Image Generator/index.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+const Jimp = require("jimp");
+
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+
+Jimp.read(config.filePath, function(err, image) {
+    if (err) { throw err }
+
+    let pixelSize = config.pixelSize;
+    if (!config.useAbsolutePixelSize) {
+        pixelSize = config.imageWidth / image.bitmap.width;
+    }
+
+    compile(image, pixelSize);
+});
+
+function compile(image, pixelSize) {
+    debugDump(image);
+    let stylesheetString = '';
+    for (let y = 1; y <= image.bitmap.height; y++) {
+        for (let x = 1; x <= image.bitmap.width; x++) {
+            let pixel = Jimp.intToRGBA(image.getPixelColour(x - 1, y - 1));
+            stylesheetString += `${x*pixelSize}px ${y*pixelSize}px 0px 0px rgba(${pixel.r}, ${pixel.g}, ${pixel.b}, ${pixel.a}),`;
+        }
+    }
+    stylesheetString = stylesheetString.substring(0, stylesheetString.length - 1);
+
+    debugDump(stylesheetString);
+    stylesheetString = `
+.gen-pic{
+    width: ${pixelSize}px;
+    height: ${pixelSize}px;
+    box-shadow: ${stylesheetString}\n
+}`;
+    fs.writeFileSync('./stylesheet.css', stylesheetString);
+    fs.writeFileSync('./integrated.html', `
+<!DOCTYPE html>
+<html>
+
+<style>
+${stylesheetString}
+</style>
+
+<body style="background-color: #222426;">
+    <div class="gen-pic" style="outline: #d3cfca 1px solid;"></div>
+</body>
+
+</html>
+    `);
+}
+
+function debugDump(data) {
+    if (config.consoleDebugDump) {
+        console.log(data);
+    }
+}
