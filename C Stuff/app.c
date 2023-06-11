dc8ac61a91677e16a70f8ac853495642543661ce
CCheukKa <ccheukka@gmail.com>
2023-06-12 00:06:40 +0800

    added C project

--- C Stuff/app.c
+++ C Stuff/app.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-int main(void){
-    float x1, y1, x2, y2, x3, y3;
-    scanf("%f %f %f %f %f %f", &x1, &y1, &x2, &y2, &x3, &y3);
+int main() {
+    int a = 0;
+    scanf("%d", &a);
+    printf("%d", a);
+    printf("printed\n");
 
-    float area = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2;
-    if(area < 0){
-         area *= -1; }
-    printf("%.3f", area);
     return 0;
 }
